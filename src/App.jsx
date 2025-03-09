593f1d6200ef203c79257daa67ceeda5e324c7b0
Raanesh01 <raaneshu@gmail.com>
2025-03-09 19:08:34 +0530

    Initial commit of Brainwave website

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefits from "./Components/Benefits";
+import Header from "./Components/Header";
+import Hero from "./Components/Hero";
+import Collaboration from "./Components/Collaboration";
+import Services from "./Components/Services";
+import Pricing from "./Components/Pricing";
+import Roadmap from "./Components/Roadmap";
+import Footer from "./Components/Footer";
+
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+        <Benefits />
+        <Collaboration />
+        <Services />
+        <Pricing />
+        <Roadmap />
+        <Footer />
+
+      </div>
+
+      <ButtonGradient />
+      </>
+  )
+}
+
+
+export default App
